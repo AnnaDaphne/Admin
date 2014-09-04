140d184693ab9532c5a2c55fa3cef1116aab3230
MrSaints <ian@fyianlai.com>
2014-09-05 01:50:11 +0800

    Minor refactor (registered in models.go); modified field names; and added validation definitions

--- models/places.go
+++ models/places.go
@@ -1,24 +1,15 @@
 package models
 
-import (
-    "github.com/astaxie/beego/orm"
-    //_ "github.com/go-sql-driver/mysql"
-)
-
 type Place struct {
     Id          int     `form:"-"`
-    Name        string  `form:"name"`
-    Description string  `form:"description"`
+    Name        string  `form:"name" valid:"Required;AlphaNumeric;MinSize(3);MaxSize(50)"`
+    Description string  `form:"description" valid:"MaxSize(600)"`
     Website     string  `form:"website"`
-    Lat         string  `form:"gpsLat" orm:type(POINT)`
-    Long        string  `form:"gpsLong" orm:type(POINT)`
+    Latitude    float64 `form:"latitude"`
+    Longitude   float64 `form:"longitude"`
     Type        int     `form:"-"`
 }
 
 func (p *Place) TableName() string {
     return "places"
-}
-
-func init() {
-    orm.RegisterModel(new(Place))
 }
