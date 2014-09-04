1e9c270abdc6847939d721452083d3e5c5a26441
MrSaints <ian@fyianlai.com>
2014-09-05 01:47:54 +0800

    Fixed indentation

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	_ "github.com/mrsaints/AD-ACP/routers"
-	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/orm"
-	_ "github.com/go-sql-driver/mysql"
+    _ "github.com/mrsaints/AD-ACP/routers"
+    "github.com/astaxie/beego"
+    "github.com/astaxie/beego/orm"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
-	orm.RegisterDriver("mysql", orm.DR_MySQL)
-	orm.RegisterDataBase("default", "mysql", "root:123456@/annadaphne?charset=utf8")
+    /*
+     * TODO: Config
+     */
+    orm.RegisterDriver("mysql", orm.DR_MySQL)
+    orm.RegisterDataBase("default", "mysql", "root:123456@/annadaphne?charset=utf8")
 }
 
 func main() {
-	beego.Run()
+    beego.Run()
 }
 
